b70d07def3ded866cf2d4196491fde2d6b28516a
kite3 <iskit3@126.com>
2020-10-02 11:57:44 +0800

    客户端数据预取

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,13 @@
 <template>
   <div>
-    <a href="javascript:void(0)" @click.prevent="goHome">home</a>
-    <a href="javascript:void(0)" @click.prevent="goAbout">about</a>
+    <router-link to="/">home</router-link>
+    <router-link to="/about">about</router-link>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  methods: {
-    goHome() {
-      location.href = location.origin + '/'
-    },
-    goAbout() {
-      location.href = location.origin + '/about'
-    },
-  },
-}
+export default {}
 </script>
 
 <style></style>
