2a54380841e0c77bd6d50e07ceacde254856f01c
kite3 <iskit3@126.com>
2020-10-02 11:02:06 +0800

    移除注释 + fix挂载时找不到id的问题

--- server.js
+++ server.js
@@ -2,18 +2,13 @@ const Vue = require('vue')
 const path = require('path')
 const express = require('express')
 const server = express()
-server.use(express.static('dist'));
-// TODO
-// const createApp = require('/path/to/built-server-bundle.js')
-
-const template = require('fs').readFileSync('./index.template.html', 'utf-8')
-const serverBundle = require('./dist/vue-ssr-server-bundle.json')
-const clientManifest = require('./dist/vue-ssr-client-manifest.json')
-
-// const render = require('vue-server-renderer').createRenderer({
-//   template,
-// })
 const { createBundleRenderer } = require('vue-server-renderer')
+
+server.use(express.static('dist'))
+
+const serverBundle = require('./dist/vue-ssr-server-bundle.json')
+const template = require('fs').readFileSync('./index.template.html', 'utf-8')
+const clientManifest = require('./dist/vue-ssr-client-manifest.json')
 
 const renderer = createBundleRenderer(serverBundle, {
   runInNewContext: false, // 推荐
@@ -23,22 +18,6 @@ const renderer = createBundleRenderer(serverBundle, {
 
 server.get('*', (req, res) => {
   const context = { url: req.url }
-
-  // createApp(context).then((app) => {
-  //   render.renderToString(app, context, (err, html) => {
-  //     if (err) {
-  //       if (err.code === 404) {
-  //         res.status(404).end('Page not found')
-  //       } else {
-  //         res.status(500).end('Internal Server Error')
-  //       }
-  //     } else {
-  //       res.set('Content-Type', 'text/html')
-  //       res.end(html)
-  //     }
-  //   })
-  // })
-
   // 这里无需传入一个应用程序，因为在执行 bundle 时已经自动创建过。
   // 现在我们的服务器与应用程序已经解耦！
   renderer.renderToString(context, (err, html) => {
@@ -49,7 +28,9 @@ server.get('*', (req, res) => {
         res.status(500).end('Internal Server Error')
       }
     } else {
-      res.set('Content-Type', 'text/html')
+      // 添加$mount所需的id
+      html = html.replace(`data-server-rendered="true"`, `id="app" data-server-rendered="true"`)
+      // console.log(html)
       res.end(html)
     }
   })
