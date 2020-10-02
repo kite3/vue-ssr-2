89dc3055a4f155021eda230a278f640f7d4c9c52
kite3 <iskit3@126.com>
2020-10-02 11:17:18 +0800

    测试home page数据预取

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,29 @@
 <template>
-  <h1 @click="onClick">this is home page</h1>
+  <div>
+    <h1 @click="onClick">this is home page</h1>
+    <ul>
+      <li v-for="item in items" :key="item.name">
+        名字：{{ item.name }}，年龄：{{ item.age }}岁
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
+  asyncData({ store, route }) {
+    return store.dispatch('fetchItems')
+  },
+  computed: {
+    items() {
+      return this.$store.state.items
+    },
+  },
   methods: {
     onClick() {
       console.log('home')
-    }
-  }
+    },
+  },
 }
 </script>
 
--- src/store.js
+++ src/store.js
@@ -5,25 +5,36 @@ Vue.use(Vuex)
 
 // 假定我们有一个可以返回 Promise 的
 // 通用 API（请忽略此 API 具体实现细节）
-const fetchItem = Promise.resolve({ name: 'zhangsan', age: 25 })
+const fetchItems = function () {
+  return new Promise((resolve, reject) => {
+    const items = [
+      { name: 'zhangsan', age: 25 },
+      { name: 'lisi', age: 26 },
+      { name: 'wangwu', age: 27 }
+    ]
+    setTimeout(() => {
+      resolve(items)
+    }, 300)
+  })
+}
 
 export function createStore() {
   return new Vuex.Store({
     state: {
-      items: {},
+      items: [],
     },
     actions: {
-      fetchItem({ commit }, id) {
+      fetchItems({ commit }) {
         // `store.dispatch()` 会返回 Promise，
         // 以便我们能够知道数据在何时更新
-        return fetchItem(id).then((item) => {
-          commit('setItem', { id, item })
+        return fetchItems().then((items) => {
+          commit('setItems', items)
         })
       },
     },
     mutations: {
-      setItem(state, { id, item }) {
-        Vue.set(state.items, id, item)
+      setItems(state, items) {
+        this.state.items = items
       },
     },
   })
